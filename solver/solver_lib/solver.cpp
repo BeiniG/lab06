97282bcbe32078e62db06e7337a59c1db717a5ae
vboxuser <vboxuser@debian.myguest.virtualbox.org>
2026-05-18 18:23:05 +0300

    added files

---
+++ solver/solver_lib/solver.cpp
@@ -0,0 +1,16 @@
+#include "solver.h"
+#include <cmath>
+#include <stdexcept>
+
+void solve(float a, float b, float c, float& x1, float& x2)
+{
+    float d = (b * b) - (4 * a * c);
+
+    if (d < 0)
+    {
+        throw std::logic_error{"Discriminant must be equal or more than 0"};
+    }
+
+    x1 = (-b - std::sqrt(d)) / (2 * a);
+    x2 = (-b + std::sqrt(d)) / (2 * a);
+}
